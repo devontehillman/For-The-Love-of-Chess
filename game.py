8d92906b37a2b8119c5325b008ed617f2ce4b394
devontehillman <hillmand@mail.gvsu.edu>
2023-04-13 06:35:26 -0400

    merged various functions

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
+import copy
 from piece_model import *
 
-class stack():
+class Stack():
         """
         holds prior board states to be utilized by redo/undo
         """
@@ -30,6 +31,7 @@ class Game():
         self._board = [[None for _ in range(8)] for _ in range(8)]
         self.current_player = Color.WHITE
         self._setup_pieces()
+        self._B = Stack()
 
     
         
@@ -37,36 +39,38 @@ class Game():
         """
         resets the board and pieces to default state
         """
+        while not self._B.empty():
+            self._B.pop()
 
     def _setup_pieces(self):
         """
         Initializes board by starting all pieces in their correct positions
         """
-        self._board[0][0] = Rook(Color['BLACK'])
-        self._board[0][1] = Knight(Color['BLACK'])
-        self._board[0][2] = Bishop(Color['BLACK'])
-        self._board[0][3] = Queen(Color['BLACK'])
-        self._board[0][4] = King(Color['BLACK'])
-        self._board[0][5] = Bishop(Color['BLACK'])
-        self._board[0][6] = Knight(Color['BLACK'])
-        self._board[0][7] = Rook(Color['BLACK'])
+        self._board[0][0] = Rook(Color['BLACK'], self._board)
+        #self._board[0][1] = Knight(Color['BLACK'], self._board)
+        self._board[0][2] = Bishop(Color['BLACK'], self._board)
+        self._board[0][3] = Queen(Color['BLACK'], self._board)
+        self._board[0][4] = King(Color['BLACK'], self._board)
+        self._board[0][5] = Bishop(Color['BLACK'], self._board)
+        #self._board[0][6] = Knight(Color['BLACK'], self._board)
+        self._board[0][7] = Rook(Color['BLACK'], self._board)
         
         
-        for i in range(8):
-            self._board[1][i] = Pawn(Color['BLACK'])
+        """for i in range(8):
+            self._board[1][i] = Pawn(Color['BLACK'],  self._board)
+        """
+        self._board[7][0] = Rook(Color['WHITE'], self._board)
+        #self._board[7][1] = Knight(Color['WHITE'], self._board)
+        self._board[7][2] = Bishop(Color['WHITE'], self._board)
+        self._board[7][4] = Queen(Color['WHITE'], self._board)
+        self._board[7][3] = King(Color['WHITE'], self._board)
+        self._board[7][5] = Bishop(Color['WHITE'], self._board)
+        #self._board[7][6] = Knight(Color['WHITE'], self._board)
+        self._board[7][7] = Rook(Color['WHITE'], self._board)
         
-        self._board[7][0] = Rook(Color['WHITE'])
-        self._board[7][1] = Knight(Color['WHITE'])
-        self._board[7][2] = Bishop(Color['WHITE'])
-        self._board[7][4] = Queen(Color['WHITE'])
-        self._board[7][3] = King(Color['WHITE'])
-        self._board[7][5] = Bishop(Color['WHITE'])
-        self._board[7][6] = Knight(Color['WHITE'])
-        self._board[7][7] = Rook(Color['WHITE'])
         
-        
-        for i in range(8):
-            self._board[6][i] = Pawn(Color['WHITE'])
+        """for i in range(8):
+            self._board[6][i] = Pawn(Color['WHITE'], self._board)"""
 
 
 
@@ -91,7 +95,12 @@ class Game():
         Pops the last board state from the stack and set the current board to it
         Return true if this can be done and false if there is no prior state
         """
-        pass
+        if self._B.empty():
+            return False
+        else:
+            self._B.pop()
+            self._board = self._B.peek()
+            return True
 
     def copy_board(self):
         """
@@ -99,49 +108,85 @@ class Game():
         the AI to explore possible moves without affecting the current board. 
         Copying a board must be a deep copy - not a shallow copy. 
         """
-        pass
+        self._prior = copy.deepcopy(self._board)
+        self._boardStack.push(self._prior)
 
     def move(self, piece: Piece, y: int, x: int, y2: int, x2: int) -> bool:
         """
-        copy the board into the prior states stack then perform the move by setting 
-        the new location to the piece, and removing the piece from the old location. 
+        1. copy the board into the prior states stack 
+        2. then perform the move by setting the new location(y2,x2) to the piece, 
+        and removing the piece from the old location(y,x). 
         
-        If the move was made by a Pawn , then the pawn should be
+        #3If the move was made by a Pawn , then the pawn should be
         updated to reflect that it is no longer the pawn's first move. 
         
-        You must then determine if the
-        move resulted in the current player being placed in check, and undo the move if it does.
+        #4You must then determine if the move resulted in the current player 
+        being placed in check, and undo the move if it does.
 
         No player should be allowed to perform a move that places themselves in check.
 
         If the piece is a Pawn  and the new location is the opposite side of the board, the 
         pawn should be removed and a Queen  of the same color placed in its location.
         """
-        pass
+        #1 
+        self._B.push(self._board)
+        #2
+        self._board[y][x] = None 
+        self._board[y2][x2] = piece
+        #3 pawn already takes care of this
+        #4 Check where king is and then for all opposing pieces check if its location is in their valid moves. 
+        #Essentially if you placed your self into check
+        if self.check(piece.color):
+            self.undo()
+        
+
     
     def get_piece_locations(self, color: Color) -> list[tuple[int, int]]:
         """
         This will return the (y, x)  locations (as a tuple) for all the pieces on the board
         of the given color.
         """
-        pass
+        locations = []
+        for y in self._board:
+            for x in y:
+                if self.color == color:
+                    locations.append((y, x))
+
+        return locations
 
     def find_king(self, color: Color) -> tuple[int, int]:
         """
-        This will find the position of the King  of the given color.
+        This will find the position of the King of the given color.
         """
-        _
+        for row in self._board:
+            for piece in row:
+                if isinstance(piece, King):
+                    if piece._color == color:
+                        return (piece)
 
     def check(self, color: Color) -> bool:
         """
         This method will get locations of the opposing pieces (use the method above). 
         It will then iterate over each of those pieces, calling their valid_moves  method, 
         and adding all valid moves for all pieces to a list of possible moves. The 
-        method should then get the position of the king. If the positionof the king is in 
+        method should then get the position of the king. If the position of the king is in 
         the list of possible moves, the king is in check. Return True  in this case,
         and False  otherwise.
         """
-        pass
+        total_opposing_team_moves = []
+        
+        for piece in self.get_piece_locations(color):
+            row =  piece[0]
+            col =  piece[1]
+            actual_piece = self.board[row][col]
+            
+            total_opposing_team_moves += [actual_piece.valid_moves()]
+        
+        if self.find_king(color) in total_opposing_team_moves:
+            return True
+        else:
+            return False
+
 
     def _computer_move(self):
         """
